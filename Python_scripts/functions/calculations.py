131a4637bb132fd6512376c212a9c53d7d6c88db
Galytska <egalytska@iup.physik.uni-bremen.de>
2022-08-12 15:50:29 +0200

    EG helper function

---
+++ Python_scripts/functions/calculations.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+#Function to compute F1-score from p_matrices and val_matrices
+def get_metric_f1(ref_p_matrix, p_matrix, ref_val_matrix, val_matrix, alpha, 
+            tau_min=0, tau_diff=1, same_sign=True):
+
+    N, N, taumaxp1 = val_matrix.shape
+    TP = 0
+    FP = 0
+    FN = 0
+    auto = 0
+    count = 0
+    for i in range(N):
+        for j in range(N):
+            if i != j:
+                for tau in range(tau_min, taumaxp1):
+#                     print(np.sum(ref_p_matrix[i,j,tau] < alpha),np.sum(p_matrix[i,j,tau] < alpha))
+                    if ref_p_matrix[i,j,tau] > alpha and p_matrix[i,j,tau] < alpha:
+                        FP += 1
+                    elif ref_p_matrix[i,j,tau] < alpha and np.any(p_matrix[i,j,max(0,tau-tau_diff):tau+tau_diff+1] < alpha):
+                        count +=1
+                        if same_sign==True and np.sign(ref_val_matrix[i,j,tau]) == np.sign(val_matrix[i,j,tau]):
+                            TP += 1
+                        elif same_sign==True and np.sign(ref_val_matrix[i,j,tau]) != np.sign(val_matrix[i,j,tau]):
+                            FN += 1
+                        elif same_sign==False:
+                            TP += 1
+                    elif ref_p_matrix[i,j,tau] < alpha and not(np.any(p_matrix[i,j,max(0,tau-tau_diff):tau+tau_diff+1] < alpha)):
+                        FN += 1
+            else:
+                auto +=1
+    precision =  float(TP+1e-10) / float(TP + FP +1e-10)
+    recall = float(TP+1e-10) / float(TP + FN +1e-10)
+    f1 = 2.0*precision*recall/float(precision + recall)
+    return precision, recall, TP, FP, FN, f1, auto, count
